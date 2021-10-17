aed73b9823343131d0e0ed4ee2f3ad48fef49e46
samaritanodev <lesly.samaritano.ayala@gmail.com>
2021-10-17 17:37:49 -0500

    header com ola doble

--- apppapeldeparede/lib/src/widgets/headers.dart
+++ apppapeldeparede/lib/src/widgets/headers.dart
@@ -149,3 +149,91 @@ class _HeaderPicoPainter extends CustomPainter {
     //throw UnimplementedError();
   }
 }
+
+//Header Curvo
+class HeaderCurvo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderCurvoPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderCurvoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = new Paint();
+
+    //propiedades del lapiz
+    paint.color = Color(0xffCD60DA);
+    paint.style = PaintingStyle.fill; //fill despues
+    paint.strokeWidth = 20;
+
+    final path = new Path();
+
+    //dibujar con el path y paint
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width * 0.5, size.height * 0.4, size.width, size.height * 0.25);
+    //los dos primeros coordenadas es el eje por donde pasa la curvatura
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+    //throw UnimplementedError();
+  }
+}
+
+//Header weave
+class HeaderWeave extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderWeavePainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWeavePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = new Paint();
+
+    //propiedades del lapiz
+    paint.color = Color(0xffCD60DA);
+    paint.style = PaintingStyle.fill; //fill despues
+    paint.strokeWidth = 20;
+
+    final path = new Path();
+
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(
+      size.width * 0.25, size.height * 0.30,
+      size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(
+      size.width * 0.75, size.height * 0.20, 
+      size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+    //throw UnimplementedError();
+  }
+}
