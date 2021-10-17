a90458654ace4cac03982d92494a4fa77a9d2160
samaritanodev <lesly.samaritano.ayala@gmail.com>
2021-10-17 16:47:19 -0500

    header com pico

--- apppapeldeparede/lib/src/widgets/headers.dart
+++ apppapeldeparede/lib/src/widgets/headers.dart
@@ -107,3 +107,45 @@ class _HeaderTriangularPainter extends CustomPainter {
     //throw UnimplementedError();
   }
 }
+
+//header pico
+class HeaderPico extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderPicoPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderPicoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = new Paint();
+
+    //propiedades del lapiz
+    paint.color = Color(0xffCD60DA);
+    paint.style = PaintingStyle.fill; //fill despues
+    paint.strokeWidth = 20;
+
+    final path = new Path();
+
+    //dibujar con el path y paint
+    path.lineTo(0, size.height * 0.2);
+    path.lineTo(size.width * 0.5, size.height * 0.3);
+    path.lineTo(size.width, size.height * 0.2);
+    path.lineTo(size.width, 0);
+    //canvas dibujae el path
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+    //throw UnimplementedError();
+  }
+}
