05d764232da0f7a41ea04b7e7057bbfa33ad7a82
samaritanodev <lesly.samaritano.ayala@gmail.com>
2021-10-17 16:30:15 -0500

    header diagonal largo

--- apppapeldeparede/lib/src/widgets/headers.dart
+++ apppapeldeparede/lib/src/widgets/headers.dart
@@ -54,8 +54,49 @@ class _HeaderDiagonalPainter extends CustomPainter {
     path.moveTo(0, size.height * 0.35);
     path.lineTo(size.width, size.height * 0.30);
     path.lineTo(size.width, 0);
-    path.lineTo(0,0);
-    
+    path.lineTo(0, 0);
+
+    //canvas dibujae el path
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+    //throw UnimplementedError();
+  }
+}
+
+//header triangular
+class HeaderTriangular extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderTriangularPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderTriangularPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = new Paint();
+
+    //propiedades del lapiz
+    paint.color = Color(0xffCD60DA);
+    paint.style = PaintingStyle.fill; //fill despues
+    paint.strokeWidth = 20;
+
+    final path = new Path();
+
+    //dibujar con el path y paint
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+
     //canvas dibujae el path
     canvas.drawPath(path, paint);
   }
