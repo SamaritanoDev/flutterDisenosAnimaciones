834b8be9c79deebc19a4a4bb18de8467c0fd4944
samaritanodev <lesly.samaritano.ayala@gmail.com>
2021-10-17 16:05:41 -0500

    header diagonal

--- apppapeldeparede/lib/src/pages/header_page.dart
+++ apppapeldeparede/lib/src/pages/header_page.dart
@@ -7,7 +7,7 @@ class HeadersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HeaderBorderRedondeados(),
+      body: HeaderDiagonal(),
     );
   }
 }
--- apppapeldeparede/lib/src/widgets/headers.dart
+++ apppapeldeparede/lib/src/widgets/headers.dart
@@ -24,3 +24,45 @@ class HeaderBorderRedondeados extends StatelessWidget {
         ));
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = new Paint();
+
+    //propiedades del lapiz
+    paint.color = Color(0xffCD60DA);
+    paint.style = PaintingStyle.fill; //fill despues
+    paint.strokeWidth = 20;
+
+    final path = new Path();
+
+    //dibujar con el path y paint
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0,0);
+    
+    //canvas dibujae el path
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+    //throw UnimplementedError();
+  }
+}
