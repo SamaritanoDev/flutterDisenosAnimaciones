f3e597eba7c65b3f8981cbaefbaf479bf8f63e5a
samaritanodev <lesly.samaritano.ayala@gmail.com>
2022-01-12 21:56:52 -0500

    create SingleTickerProviderStateMixin

--- appanimation/lib/pages/animaciones_page.dart
+++ appanimation/lib/pages/animaciones_page.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 
 class AnimacionesPage extends StatelessWidget {
-  const AnimacionesPage({ Key? key }) : super(key: key);
+  const AnimacionesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: _Rectangulo(),
+        child: CuadradoAnimado(),
       ),
     );
   }
 }
 
-
 class _Rectangulo extends StatelessWidget {
-  const _Rectangulo({ Key? key }) : super(key: key);
+  const _Rectangulo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,5 +25,40 @@ class _Rectangulo extends StatelessWidget {
         color: Colors.deepPurpleAccent,
       ),
     );
+  }
+}
+
+class CuadradoAnimado extends StatefulWidget {
+  const CuadradoAnimado({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CuadradoAnimado> createState() => _CuadradoAnimadoState();
+}
+
+class _CuadradoAnimadoState extends State<CuadradoAnimado>
+    with SingleTickerProviderStateMixin {
+  AnimationController? controller;
+  Animation<double>? rotacion;
+
+  @override
+  void initState() {
+    //vsync el cuadrarp tenga su resolucion adecuado, usa el mixin
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 4000));
+    rotacion = Tween(begin: 0.0, end: 2.0).animate(controller);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: _Rectangulo());
   }
 }
