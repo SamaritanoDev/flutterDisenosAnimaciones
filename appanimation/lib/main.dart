a35e0d9b6d9e2cfef4c015814765cfa1d3757586
samaritanodev <lesly.samaritano.ayala@gmail.com>
2022-01-19 17:57:24 -0500

    corrigiendo el anomalo con late

--- appanimation/lib/main.dart
+++ appanimation/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Headers',
+      title: 'Animation',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
--- appanimation/lib/pages/animaciones_page.dart
+++ appanimation/lib/pages/animaciones_page.dart
@@ -13,20 +13,6 @@ class AnimacionesPage extends StatelessWidget {
   }
 }
 
-class _Rectangulo extends StatelessWidget {
-  const _Rectangulo({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 70,
-      height: 70,
-      decoration: const BoxDecoration(
-        color: Colors.deepPurpleAccent,
-      ),
-    );
-  }
-}
 
 class CuadradoAnimado extends StatefulWidget {
   const CuadradoAnimado({
@@ -39,20 +25,24 @@ class CuadradoAnimado extends StatefulWidget {
 
 class _CuadradoAnimadoState extends State<CuadradoAnimado>
     with SingleTickerProviderStateMixin {
-  AnimationController? controller;
-  Animation<double>? rotacion;
 
+  late AnimationController controller;
+  Animation<double>? rotacion;
+  //instanciar
   @override
   void initState() {
     //vsync el cuadrarp tenga su resolucion adecuado, usa el mixin
     controller = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 4000));
+        vsync: this, duration: const Duration(milliseconds: 4000)
+    );
+
     rotacion = Tween(begin: 0.0, end: 2.0).animate(controller);
     super.initState();
   }
 
   @override
   void dispose() {
+    //limpieza
     controller.dispose();
     super.dispose();
   }
@@ -60,5 +50,20 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
   @override
   Widget build(BuildContext context) {
     return const Center(child: _Rectangulo());
+  }
+}
+
+class _Rectangulo extends StatelessWidget {
+  const _Rectangulo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: const BoxDecoration(
+        color: Colors.deepPurpleAccent,
+      ),
+    );
   }
 }
